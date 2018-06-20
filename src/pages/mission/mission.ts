e13b68e80008dbaac041dd7f080055aae4a90603
Igor Moraes <igor.sgm@gmail.com>
2018-06-20 19:20:38 -0300

    == MissionPage + Implemented the method of accepting a mission by credit (instant-completion) or by normal item delivery + acceptMission method implemented: the method to accept a mission + showMissionAlert method implemented: Method for displaying an alert after mission accepting or failure while accepting.

--- src/pages/mission/mission.ts
+++ src/pages/mission/mission.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Storage} from "@ionic/storage";
+import {RestProvider} from "../../providers/rest/rest";
+import {HttpClient} from "@angular/common/http";
 
 @IonicPage()
 @Component({
@@ -8,16 +11,54 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class MissionPage {
 
+	user: any;
 	item: any;
 	shelter: any;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	constructor(public navCtrl: NavController, public navParams: NavParams, public localStorage: Storage, public restProvider: RestProvider, public http: HttpClient, public alertCtrl: AlertController) {
 		this.item = this.navParams.data.mission;
 		this.shelter = this.navParams.data.shelter;
+
+		this.localStorage.get('user').then(user => {
+			this.user = user;
+		});
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad MissionPage');
 	}
 
+	/**
+	 * Method to accept a mission
+	 *
+	 * @param {string}    completionType    'item' or 'credit'
+	 */
+	acceptMission(completionType) {
+		let url = this.restProvider.getUrlApi() + '/completion/AcceptMission?mission_id=' + this.item.mission.id + '&hero_id=' + this.user.id + '&completion_type=' + completionType;
+
+		this.http.get(url, this.restProvider.getHeadersJson()).toPromise()
+			.then((data: any) => {
+				let title = data.status === true ? 'Mission accepted' : 'Error';
+				this.showMissionAlert(title, data.message);
+			}).catch((error) => {
+			this.showMissionAlert('Error', 'Failed to accept mission, please try again later');
+		});
+	}
+
+	/**
+	 * Method for displaying an alert after mission accepting or failure while accepting.
+	 *
+	 * @param {string}    title        Alert title
+	 * @param {string}    message        Alert message
+	 */
+	public showMissionAlert(title, message) {
+
+		let alert = this.alertCtrl.create({
+			title: title,
+			subTitle: message,
+			buttons: ['OK']
+		});
+
+		alert.present();
+	}
 }
