adefc9cd3b59fefe56e4fa8f4281a4bed3871d3c
Igor Moraes <igor.sgm@gmail.com>
2018-06-25 15:27:37 -0300

    == ShelterPage + getShelterReviews, showReviewAlert and setRating methods created + Shelter Reviews functionality. Now when thw "review" button is clicked, a popup with a text field is displayed and when the page is submitted its updated with the comments data.

--- src/pages/shelter/shelter.ts
+++ src/pages/shelter/shelter.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import {Storage} from "@ionic/storage";
 import {RestProvider} from "../../providers/rest/rest";
 import {HttpClient} from "@angular/common/http";
@@ -13,17 +13,19 @@ export class ShelterPage {
 
 	user: any;
 	shelter: any;
+	reviews: Array<any> = [];
 
-	constructor(public navCtrl: NavController, public navParams: NavParams, public localStorage: Storage, public restProvider: RestProvider, public http: HttpClient) {
+	constructor(public navCtrl: NavController, public navParams: NavParams, public localStorage: Storage, public viewCtrl: ViewController,
+				public restProvider: RestProvider, public http: HttpClient, public alertCtrl: AlertController) {
 
 		this.localStorage.get('user').then(user => {
 			this.user = user;
 			this.getShelterDetails(this.navParams.data.shelter_id);
+			this.getShelterReviews(this.navParams.data.shelter_id);
 		});
 
 		this.shelter = this.navParams.data.shelter;
 
-		console.log(this.shelter);
 	}
 
 	ionViewDidLoad() {
@@ -31,7 +33,6 @@ export class ShelterPage {
 	}
 
 	getShelterDetails(idShelter) {
-
 		if (idShelter !== null) {
 			let body = {
 				shelter_id: idShelter
@@ -41,6 +42,69 @@ export class ShelterPage {
 				.then((data: any) => {
 					console.log(data);
 					this.shelter = data;
+				}).catch((error) => {
+				console.log(error);
+			});
+		}
+	}
+
+	getShelterReviews(idShelter) {
+		if (idShelter !== null) {
+			let body = {
+				shelter_id: idShelter
+			};
+
+			this.http.post(this.restProvider.getUrlApi() + '/shelter/GetShelterReviews', body, this.restProvider.getHeadersUrlEncoded()).toPromise()
+				.then((data: any) => {
+					Object.keys(data).forEach((key, index) => {
+						this.reviews.push(data[key]);
+					});
+				}).catch((error) => {
+				console.log(error);
+			});
+		}
+	}
+
+	showReviewAlert() {
+		let alert = this.alertCtrl.create({
+			title: 'Rate this Shelter!',
+			inputs: [
+				{
+					name: 'review',
+					placeholder: 'Write a review'
+				}
+			],
+			buttons: [
+				{
+					text: 'Rate it!',
+					handler: (data) => {
+						this.setRating(data.review);
+					}
+				}
+			]
+		});
+		alert.present();
+	}
+
+	setRating(review) {
+		if (review !== '' && review !== null) {
+			let body = {
+				shelter_id: this.shelter.shelter_id,
+				user_id: this.user.id,
+				rating: '5',
+				review: review
+			};
+
+			this.http.post(this.restProvider.getUrlApi() + '/shelter/setRating', body, this.restProvider.getHeadersUrlEncoded()).toPromise()
+				.then((data: any) => {
+					console.log(data);
+
+					this.navCtrl.push('ShelterPage', {
+						shelter_id: this.shelter.shelter_id
+					}, {animate: false}).then(() => {
+						this.navCtrl.remove(this.viewCtrl.index);
+					});
+
 				}).catch((error) => {
 				console.log(error);
 			});
